3a24932073d0738b6a719317bff0d8a93e7d9760
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2021-11-27 02:09:50 +0200

    add prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    css: ['tailwindcss/tailwind.css'],
-    build: {
-        postcss: {
-            postcssOptions: require('./postcss.config.js'),
-        },
-    },
-})
+	css: ["tailwindcss/tailwind.css"],
+	build: {
+		postcss: {
+			postcssOptions: require("./postcss.config.js"),
+		},
+	},
+});
