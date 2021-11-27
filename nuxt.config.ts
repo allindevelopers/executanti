d7fbac33fdf76b2fc54d61c5e66dff6416ad9282
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2021-11-27 03:02:35 +0200

    add blockly example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,33 @@
 import { defineNuxtConfig } from "nuxt3";
+import CopyPlugin from 'copy-webpack-plugin';
+import path from 'path'
+
+const elements = ['field', 'block', 'category', 'xml', 'mutation', 'value', 'sep', 'shadow']
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+	ssr: false,
 	css: ["tailwindcss/tailwind.css"],
 	build: {
 		postcss: {
 			postcssOptions: require("./postcss.config.js"),
 		},
+		plugins: [
+			new CopyPlugin({
+				patterns: [{
+					from: path.resolve(__dirname, './node_modules/blockly/media'),
+					to: path.resolve(__dirname, 'public/media')
+				}]
+			})
+		],
 	},
+	vue: {
+		compilerOptions: {
+			isCustomElement: (tag) => elements.includes(tag)
+		},
+		config: {
+			productionTip: false,
+			ignoredElements: elements
+		}
+	}
 });
