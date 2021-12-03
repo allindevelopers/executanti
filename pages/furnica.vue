02e19157772e42ab4cb2b2325514c6505c6db567
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2021-12-03 20:32:00 +0200

    add some dvijenie

--- pages/furnica.vue
+++ pages/furnica.vue
@@ -1,69 +1,139 @@
 <template>
-	<div
-		ref="grid"
-		class="p-0.5 gap-0.5 grid grid-cols-10 grid-cols-[repeat(10,50px)] grid-rows-[repeat(10,50px)]"
-	>
-		<button
-			:id="`button-${i}`"
-			v-for="(cell, i) in cells"
-			@click="editCell(i)"
-			:key="i"
-			:class="[
-				'flex items-center justify-center text-3xl focus:outline-none',
-				'border border-dashed focus:border-red-600 focus:bg-red-100',
-			]"
+	<div class="m-0.5 flex items-start gap-1">
+		<div
+			ref="grid"
+			class="p-0.5 gap-0.5 bg-gray-100 inline-grid grid-cols-10 grid-cols-[repeat(10,50px)] grid-rows-[repeat(10,50px)] shadow"
 		>
-			{{ cell }}
-		</button>
+			<button
+				v-for="(cell, i) in cells"
+				:id="`button-${i}`"
+				:key="i"
+				:class="[
+					'flex items-center justify-center text-3xl focus:outline-none',
+					'border border-dashed focus:border-red-600 focus:bg-red-100 bg-white',
+					{ 'bg-blue-100': cell.type === AntType },
+				]"
+				@focus="focusedCell = cell"
+			>
+				{{ cell.char }}
+			</button>
+		</div>
+		<div class="grid gap-0.5">
+			<Button @click="setAsAnt">Set as Ant</Button>
+			<Button @click="moveAnt('ArrowUp')">Up</Button>
+			<Button @click="moveAnt('ArrowRight')">Right</Button>
+			<Button @click="moveAnt('ArrowDown')">Down</Button>
+			<Button @click="moveAnt('ArrowLeft')">Left</Button>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-const grid = ref<HTMLElement>();
-const cells = ref(Array.from({ length: 100 }));
+const EmptyType = Symbol.for("empty");
+const CharType = Symbol.for("char");
+const AntType = Symbol.for("ant");
+type AnyType = typeof EmptyType | typeof CharType | typeof AntType;
 
-function editCell(index) {
+type Cell = { type: AnyType; char?: string };
+const getEmptyCell = (): Cell => ({ type: EmptyType, char: undefined });
+const getCharCell = (char: string): Cell => ({ type: CharType, char });
+const getAntCell = (char: string): Cell => ({ type: AntType, char });
+
+const focusedCell = ref<Cell>();
+const grid = ref<HTMLElement>();
+const cells = ref(
+	Array.from({ length: 100 }, (_, index) =>
+		54 === index ? getAntCell("🐜") : getEmptyCell()
+	)
+);
+const size = 10;
+const arrowCodes = ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft"];
+const controllCodes = ["Backspace", "Enter", "Escape"];
+const allCodes = [...arrowCodes, ...controllCodes];
+
+const DirectionMap: Record<string, (div: number, mod: number) => number> = {
+	ArrowUp: (div, mod) => wrap(div - 1, 0, size) * size + mod,
+	ArrowRight: (div, mod) => div * size + wrap(mod + 1, 0, size),
+	ArrowDown: (div, mod) => wrap(div + 1, 0, size) * size + mod,
+	ArrowLeft: (div, mod) => div * size + wrap(mod - 1, 0, size),
+};
+
+function setAsAnt() {
+	const cellIndex = cells.value.indexOf(focusedCell.value);
+	if (cellIndex < 0) return;
+
+	const remove = (cell: Cell) =>
+		cell.type !== AntType ? cell : getCharCell(cell.char);
+	const set = (cell: Cell, index: number) =>
+		index === cellIndex ? getAntCell(cell.char) : cell;
+
+	cells.value = cells.value.map(remove).map(set);
+}
+
+const MoveMap: Record<string, (div: number, mod: number) => number> = {
+	ArrowUp: (div, mod) => clamp(div - 1, 0, size - 1) * size + mod,
+	ArrowRight: (div, mod) => div * size + clamp(mod + 1, 0, size - 1),
+	ArrowDown: (div, mod) => clamp(div + 1, 0, size - 1) * size + mod,
+	ArrowLeft: (div, mod) => div * size + clamp(mod - 1, 0, size - 1),
+};
+
+function moveAnt(direction: string) {
+	const antIndex = cells.value.findIndex((cell) => cell.type === AntType);
+	if (antIndex < 0) return;
+	const { div, mod } = getDivMode(antIndex);
+	const newIndex = MoveMap[direction](div, mod);
+	if (antIndex === newIndex) return;
+	const [first, last] = sort([antIndex, newIndex]);
+	cells.value = swapItems(cells.value, first, last);
+}
+
+function getChar() {
 	const text = window.prompt();
 	if (!text) return;
-	const firstSymbol = String.fromCodePoint(text.codePointAt(0));
-	cells.value[index] =
-		firstSymbol.length !== 1 ? firstSymbol : firstSymbol.toUpperCase();
+	const [firstSymbol] = Array.from(text);
+	return firstSymbol.length !== 1 ? firstSymbol : firstSymbol.toUpperCase();
 }
 
 onMounted(() => {
 	const doc = grid.value.ownerDocument;
-	const size = 10;
-	const codes = [
-		"ArrowUp",
-		"ArrowRight",
-		"ArrowDown",
-		"ArrowLeft",
-		"Backspace",
-	];
+	function listener(e: KeyboardEvent) {
+		if (document.activeElement.parentElement === grid.value) return;
+		if (!arrowCodes.includes(e.code)) return;
+		moveAnt(e.code);
+	}
+	doc.addEventListener("keydown", listener);
+	onUnmounted(() => doc.removeEventListener("keydown", listener));
+});
 
-	const map: Record<string, (div: number, mod: number) => number> = {
-		ArrowUp: (div, mod) => rotate(div - 1, 0, size) * size + mod,
-		ArrowRight: (div, mod) => div * size + rotate(mod + 1, 0, size),
-		ArrowDown: (div, mod) => rotate(div + 1, 0, size) * size + mod,
-		ArrowLeft: (div, mod) => div * size + rotate(mod - 1, 0, size),
-	};
+onMounted(() => {
+	const doc = grid.value.ownerDocument;
 
 	function listener(e: KeyboardEvent) {
-		if (document.activeElement.parentElement !== grid.value) return;
-		if (!codes.includes(e.code)) return;
+		if (doc.activeElement.parentElement !== grid.value) return;
+		if (!allCodes.includes(e.code)) return;
 
-		const children = [...grid.value.children];
-		const focusIndex = children.indexOf(document.activeElement);
+		const children = [...grid.value.querySelectorAll("button")];
+		const focusElement = children.find((c) => c === doc.activeElement);
+		const focusIndex = children.indexOf(focusElement);
 
-		if (e.code === "Backspace") {
-			cells.value[focusIndex] = "";
+		if (e.code === "Escape") {
+			focusElement.blur();
 			return;
 		}
 
-		const div = Math.floor(focusIndex / size);
-		const mod = focusIndex % size;
-		const index = map[e.code](div, mod);
-		const selector = `:scope button:nth-child(${index + 1})`;
+		if (e.code === "Backspace") {
+			cells.value[focusIndex] = getEmptyCell();
+			return;
+		}
+
+		if (e.code === "Enter") {
+			cells.value[focusIndex] = getCharCell(getChar());
+			return;
+		}
+
+		const { div, mod } = getDivMode(focusIndex);
+		const nextIndex = DirectionMap[e.code](div, mod);
+		const selector = `:scope button:nth-child(${nextIndex + 1})`;
 		const button = grid.value.querySelector<HTMLElement>(selector);
 		button.focus();
 	}
@@ -72,9 +142,38 @@ onMounted(() => {
 	onUnmounted(() => doc.removeEventListener("keydown", listener));
 });
 
-const clamp = (val: number, min: number = 0, max: number = 1): number =>
-	Math.max(min, Math.min(max, val));
+function wrap(num: number, min: number, max: number): number {
+	const rangeSize = max - min;
+	return ((((num - min) % rangeSize) + rangeSize) % rangeSize) + min;
+}
 
-const rotate = (val: number, min: number = 0, max: number = 1): number =>
-	val < min ? val + max : val % max;
+function clamp(val: number, min: number = 0, max: number = 1): number {
+	return Math.max(min, Math.min(max, val));
+}
+
+const swapItems = <T, _>(a: T[], i: number, j: number): T[] =>
+	(a[i] &&
+		a[j] && [
+			...a.slice(0, i),
+			a[j],
+			...a.slice(i + 1, j),
+			a[i],
+			...a.slice(j + 1),
+		]) ||
+	a;
+
+const sort = (arr: number[]): number[] => arr.sort((a, b) => a - b);
+
+function getDivMode(n: number) {
+	const div = Math.floor(n / size);
+	const mod = n % size;
+	return { div, mod };
+}
+
+function Button(props: any, { slots }) {
+	const { class: className, ...rest } = props;
+	const classes = ["bg-gray-100 px-4 py-2 rounded", className];
+	const newProps = { ...rest, class: classes };
+	return h("button", newProps, slots);
+}
 </script>
