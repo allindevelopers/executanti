125d7c3bd80adddf468ecc21e4f0c80952f1df32
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2021-12-03 03:20:15 +0200

    add basic grid with editing

--- pages/furnica.vue
+++ pages/furnica.vue
@@ -1,3 +1,30 @@
 <template>
-	<h1>Furnica</h1>
+	<div
+		class="p-0.5 gap-0.5 grid grid-cols-10 grid-cols-[repeat(10,50px)] grid-rows-[repeat(10,50px)]"
+	>
+		<button
+			v-for="(cell, i) in cells"
+			:key="i"
+			:class="[
+				'flex items-center justify-center',
+				'border border-dashed text-3xl focus:outline-none focus:border-red-600 focus:bg-red-100',
+			]"
+			@click="editCell(i)"
+		>
+			{{ cell }}
+		</button>
+	</div>
 </template>
+
+<script setup>
+const cells = ref(Array.from({ length: 100 }));
+
+function editCell(index) {
+	const text = window.prompt();
+	if (!text) return;
+	const firstSymbol = String.fromCodePoint(text.codePointAt(0));
+	console.log(firstSymbol.length);
+	cells.value[index] =
+		firstSymbol.length !== 1 ? firstSymbol : firstSymbol.toUpperCase();
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
 	mode: "jit",
-	purge: [],
+	purge: {
+		content: [
+			`./components/**/*.{vue,js,ts}`,
+			`./layouts/**/*.vue`,
+			`./pages/**/*.vue`,
+			`./plugins/**/*.{js,ts}`,
+			`./nuxt.config.ts`,
+		],
+	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
